db71035371d89710661fce5e9ba67a353f0720d0
赵栩彬 <zhaoxubin@zhaoxubindeMacBook-Pro.local>
2020-05-14 09:44:09 +0800

    update

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/robfig/cron"
 	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
 	"net/smtp"
 	"net/url"
+	"robfig/cron"
 	"strconv"
 	"strings"
 	"time"
@@ -20,19 +20,20 @@ const (
 )
 
 func main() {
+
 	log.Println("Task started")
 
 	c := cron.New()
 	c.AddFunc("2 9,13,17,21 * * *", func() {
 		for _, v := range config.GetConf().Users {
 			go func() {
-				result := reportTemper(v, 3)
+				hour, min, temper1, temper2, result := reportTemper(v, 3)
 				if result == true {
 					log.Println(v.Name + " 填报成功")
 				} else {
 					log.Println(v.Name + " 填报失败")
 				}
-				sendEMail(v, result)
+				sendEMail(v, hour, min, temper1, temper2, result)
 			}()
 		}
 	})
@@ -41,24 +42,26 @@ func main() {
 	select {}
 }
 
-func reportTemper(user config.User, recount int8) bool {
-
-	if recount == 0 {
-		return false
-	}
+func reportTemper(user config.User, recount int8) (string, string, string, string, bool) {
 
 	hour, min := CurrentTime()
+	temper1 := "36"
+	temper2 := RandomAInt()
+
+	if recount == 0 {
+		return hour, min, temper1, temper2, false
+	}
 	data := make(url.Values)
 	data["TimeNowHour"] = []string{hour}
 	data["TimeNowMinute"] = []string{min}
-	data["Temper1"] = []string{"36"}
-	data["Temper2"] = []string{RandomAInt()}
+	data["Temper1"] = []string{temper1}
+	data["Temper2"] = []string{temper2}
 	data["ReSubmiteFlag"] = []string{RandomOrderId()}
 
 	req, err := http.NewRequest("POST", url_report, strings.NewReader(data.Encode()))
 	if err != nil {
 		log.Println(user.Name+"	create req failed", err)
-		return false
+		return hour, min, temper1, temper2, false
 	}
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36")
@@ -66,7 +69,7 @@ func reportTemper(user config.User, recount int8) bool {
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return false
+		return hour, min, temper1, temper2, false
 	}
 	if resp.StatusCode >= 300 {
 		log.Println(user.Name+"	Request failed", "code: ", resp.StatusCode)
@@ -75,7 +78,7 @@ func reportTemper(user config.User, recount int8) bool {
 	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Println(user.Name+"	Parse text failed", err)
-		return false
+		return hour, min, temper1, temper2, false
 	}
 	println(string(respBody))
 	success := strings.Contains(string(respBody), "成功")
@@ -84,11 +87,11 @@ func reportTemper(user config.User, recount int8) bool {
 		time.Sleep(time.Minute * time.Duration(time.Now().Hour()/3))
 		return reportTemper(user, recount-1)
 	} else {
-		return success
+		return hour, min, temper1, temper2, true
 	}
 }
 
-func sendEMail(user config.User, suc bool) error {
+func sendEMail(user config.User, timeNowHour, timeNowMinute, temper1, temper2 string, suc bool) error {
 	receviceAddr := user.Email
 	senderAddr := "1316952971@qq.com"
 	authCode := "leqkyvxagbbnhafj"
@@ -99,7 +102,10 @@ func sendEMail(user config.User, suc bool) error {
 	contentType := "Content-Type:text/plain;charset=UTF-8\r\n"
 	body := "name：" + user.Name +
 		"\nstudent id: " + user.StudentId +
+		"\nreport time: " + timeNowHour + ":" + timeNowMinute +
+		"\nreport temper: " + temper1 + "." + temper2 + " 摄氏度" +
 		"\nreport success：" + strconv.FormatBool(suc) +
+		"\nversion：V4" +
 		"\n如果上报状态失败，则可能是晚上9点尝试第四次上报，不用在乎。若是下午5点则可能当天由于学校系统重复上报，若上午9点或下午1点上报失败请联系我～"
 	msg := []byte("To: " + strings.Join(to, ",") + "\r\nFrom: " + nickname +
 		"<" + senderAddr + ">\r\nSubject: " + subject + "\r\n" + contentType + "\r\n\r\n" + body)
@@ -133,5 +139,6 @@ func RandomOrderId() string {
 }
 
 func RandomAInt() string {
-	return string(rand.New(rand.NewSource(time.Now().UnixNano())).Int31n(10))
+	randInt := rand.New(rand.NewSource(time.Now().Unix())).Intn(10)
+	return strconv.Itoa(randInt)
 }
